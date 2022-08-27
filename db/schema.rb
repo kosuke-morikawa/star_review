176feb0abbeaa7807f6fceb6714e45a8d6b45633
kosuke-morikawa <haoyousenchuan@gmail.com>
2022-08-27 09:07:22 +0900

    postモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_23_090935) do
+ActiveRecord::Schema.define(version: 2022_08_27_000340) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
