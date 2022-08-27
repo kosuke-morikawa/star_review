1c3b174c06c568c967db7416ba0fa072e647f759
kosuke-morikawa <haoyousenchuan@gmail.com>
2022-08-27 09:38:16 +0900

    postコントローラの作成
    rails g controller posts index show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
   devise_for :users
   root 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
