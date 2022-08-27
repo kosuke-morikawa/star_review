365eccaf8482f6c81c93074f1d76b0186691042a
kosuke-morikawa <haoyousenchuan@gmail.com>
2022-08-27 09:11:44 +0900

    データベースにデータを登録する(seeds.rb)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.create(name: "あああ")
+Post.create(name: "いいい")
+Post.create(name: "ううう")
+Post.create(name: "えええ")
+Post.create(name: "おおお")
