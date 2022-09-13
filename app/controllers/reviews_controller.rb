ecdbab72513344336eff9c94a66c98f9e385dc98
kosuke-morikawa <haoyousenchuan@gmail.com>
2022-09-13 23:14:32 +0900

    レビューコントローラ、createアクション

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,14 @@
 class ReviewsController < ApplicationController
   def index
   end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user_id == current_user.id
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:post_id, :score, :content)
+  end
 end
